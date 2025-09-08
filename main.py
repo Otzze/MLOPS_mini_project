30f7c757546d308ca0808d745ca844daf5ace4ec
Quentin <quentin.prunet@epita.fr>
2025-09-08 14:32:39 +0200

    feat: LEVEL 0

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-from typing import Union
-
 from fastapi import FastAPI
+from pydantic import BaseModel
+import joblib
+
+class Item(BaseModel):
+    size: float
+    nb_bedrooms: int
+    has_garden: int
 
 app = FastAPI()
 
-@app.get("/predict")
-def predict_get():
-    return {"y_pred": 2}
+model = joblib.load("regression.joblib")
+
+@app.post("/predict")
+async def predict_post(data: Item):
+    return {"y_pred": float(model.predict([[data.size, data.nb_bedrooms, data.has_garden]]))}
